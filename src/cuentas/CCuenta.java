2c25069dd8131f053fd3b65d6c8ca64a97bdd339
javibroz <106708088+javibroz@users.noreply.github.com>
2024-02-19 19:11:27 +0100

    Segundo commit desde Netbeans. Encapsulados 3/4 atributos de la clase CCuenta. Falta uno.

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -14,9 +14,41 @@ public class CCuenta {
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+        this.nombre =nom;
+        this.cuenta=cue;
+        this.saldo=sal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
     }
 
     public double estado()
