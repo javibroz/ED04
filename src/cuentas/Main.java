760cfd696aa85b7da3e75c3db18682ab16895c4d
javibroz <106708088+javibroz@users.noreply.github.com>
2024-02-24 12:55:07 +0100

    Creado método operativa_cuenta()

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -6,10 +6,14 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio LÃ³pez","1000-2365-85-1230456789",2500,0);
+        cuenta1 = new CCuenta("Antonio ópez","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
+        operativa_cuenta(cuenta1);
+    }
+
+    private static void operativa_cuenta(CCuenta cuenta1) {
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
