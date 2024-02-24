d46cfaf98f2b5d9c93d3784cfd21342b88cb352d
javibroz <106708088+javibroz@users.noreply.github.com>
2024-02-24 13:46:27 +0100

    Finalizado y creado el javadoc

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,24 +1,42 @@
 package cuentas;
 
+/**
+ * La clase CCuenta representa una cuenta bancaria básica.
+ * Permite realizar operaciones como ingresos y retiros, y 
+ * proporciona métodos para acceder y modificar la información 
+ * de la cuenta.
+ * 
+ * @author Broz
+ */
 public class CCuenta {
 
+    private String nombre; // Nombre del titular de la cuenta
+    private String cuenta; // Número de cuenta
+    private double saldo; // Saldo actual de la cuenta
+    private double tipoInteres; // Tipo de interés de la cuenta
 
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInteres;
-
-    public CCuenta()
-    {
+    /**
+     * Constructor vacio de la clase CCuenta.
+     */
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        this.nombre =nom;
-        this.cuenta=cue;
-        this.saldo=sal;
-        this.tipoInteres=tipo;
+    /**
+     * Constructor parametrizado de la clase CCuenta.
+     * 
+     * @param nom    Nombre del titular de la cuenta.
+     * @param cue    Número de cuenta.
+     * @param sal    Saldo inicial de la cuenta.
+     * @param tipo   Tipo de interés de la cuenta.
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        this.nombre = nom;
+        this.cuenta = cue;
+        this.saldo = sal;
+        this.tipoInteres = tipo;
     }
+
+    // Métodos getters y setters para acceder y modificar los atributos de la cuenta
 
     public String getNombre() {
         return nombre;
@@ -52,24 +70,38 @@ public class CCuenta {
         this.tipoInteres = tipoInteres;
     }
 
-    public double estado()
-    {
+    /**
+     * Devuelve el saldo actual de la cuenta.
+     * 
+     * @return El saldo actual de la cuenta.
+     */
+    public double estado() {
         return saldo;
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /**
+     * Realiza un ingreso en la cuenta.
+     * 
+     * @param cantidad   La cantidad a ingresar en la cuenta.
+     * @throws Exception Si la cantidad es negativa.
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
+    /**
+     * Realiza un retiro de la cuenta.
+     * 
+     * @param cantidad   La cantidad a retirar de la cuenta.
+     * @throws Exception Si la cantidad es negativa o si no hay suficiente saldo.
+     */
+    public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+            throw new Exception("No se puede retirar una cantidad negativa");
+        if (estado() < cantidad)
+            throw new Exception("No hay suficiente saldo");
         saldo = saldo - cantidad;
     }
 }
--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -1,28 +1,55 @@
 package cuentas;
 
+/**
+ * La clase Main contiene el método principal (main) que se utiliza 
+ * para probar la funcionalidad de la clase CCuenta.
+ * 
+ * @author Broz
+ */
 public class Main {
 
+    /**
+     * El método principal (main) crea una instancia de la clase CCuenta, 
+     * realiza algunas operaciones con ella y muestra el saldo actual.
+     * 
+     * @param args Los argumentos de la línea de comandos (no se utilizan en este caso).
+     */
     public static void main(String[] args) {
+        // Declaración de variables
         CCuenta cuenta1;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio ópez","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        // Crear una nueva instancia de la clase CCuenta
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
 
-        operativa_cuenta(cuenta1);
+        // Obtener el saldo actual de la cuenta y mostrarlo
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es: " + saldoActual);
+
+        // Realizar operaciones en la cuenta utilizando el método operativa_cuenta
+        operativa_cuenta(cuenta1, 0);
     }
 
-    private static void operativa_cuenta(CCuenta cuenta1) {
+    /**
+     * El método operativa_cuenta realiza operaciones de retiro e ingreso en la cuenta.
+     * 
+     * @param cuenta1   La cuenta en la que se realizarán las operaciones.
+     * @param cantidad  La cantidad a ingresar en la cuenta.
+     */
+    private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
         try {
+            // Intentar retirar una cantidad de la cuenta
             cuenta1.retirar(2300);
         } catch (Exception e) {
+            // Capturar y mostrar cualquier excepción ocurrida durante el retiro
             System.out.print("Fallo al retirar");
         }
         try {
+            // Intentar ingresar una cantidad en la cuenta
             System.out.println("Ingreso en cuenta");
             cuenta1.ingresar(695);
         } catch (Exception e) {
+            // Capturar y mostrar cualquier excepción ocurrida durante el ingreso
             System.out.print("Fallo al ingresar");
         }
     }
